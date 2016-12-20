911108621f0b818326d183aae5590ad51b491a53
Alexei-117 <alexei-117@hotmail.com>
2016-12-20 02:38:34 +0100

    Comiteo bonico algo de copypaste

--- Práctica 10/registroConfirmar.php
+++ Práctica 10/registroConfirmar.php
@@ -32,16 +32,22 @@
 		7 => "Fallo al escribir el fichero al disco",
 		8 => "La subida del fichero fue detenida por la extensión");
 		
+		//Asignación de la foto
 		$ruta="img/lacara.png";
 		if(isset($_FILES["ruta"]["name"])){
 			if($_FILES["ruta"]["error"] == 0){
+				//Asignación de la ruta
 				$foto=$_FILES["ruta"]["tmp_name"];
 				$uniq = strtotime(date("Y-m-d H:i:s"));
 				$ruta="perfiles/".$_POST["nomUser"].$_FILES["ruta"]["name"];
+				
+				//Comprobación de subida
 				if(!move_uploaded_file($_FILES["ruta"]["tmp_name"],$ruta)){
 					$error=true;
 					$msgError.="<p>No se ha podido subir la foto</p>";
 				}
+				
+				//Comprobación del tipo de archivo
 				if($_FILES["ruta"]["type"] == ("image/jpeg")
 					|| $_FILES["ruta"]["type"] ==("image/gif")
 					|| $_FILES["ruta"]["type"] ==("image/png")
@@ -54,65 +60,109 @@
 					$error=true;
 					$msgError.="<p>La imagen no viene en un formato aceptado. Pruebe con una del tipo png, jpg-jpeg-jpe, bmp, gif, tiff o svg.</p>";
 				}
-				
+				//Comprobación de tamaño
 				if(ceil($_FILES["ruta"]["size"]/(1024*1024))>50){
 					$error=true;
 					$msgError.="<p>Archivo demasiado grande, suba uno más pequeño.</p>";
 				}
 				
 			}else{
-				$msgError.=$errorSubida[$_FILES["ruta"]["error"]];
+				if(!$_FILES["ruta"]["error"] == 4){
+					$msgError.=$errorSubida[$_FILES["ruta"]["error"]];
+				}
 			}
 		}
 		
-		
+		//Comprobación de fecha
 		if(isset($_POST["fecha"])){
-			$fecha=explode("-",$_POST["fecha"]);
+			/*if(strpos($_POST["fecha"],"-") !== false){
+				$fecha=explode("-",$_POST["fecha"]);
+			}
+			if(strpos($_POST["fecha"],"//") !== false){
+				$fecha=explode("//",$_POST["fecha"]);
+			}
 			if(sizeof($fecha)==3){
 				$newFecha=$fecha[2]."-".$fecha[1]."-".$fecha[0];
 				$expreg="/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/";
-				if(!preg_match($expreg,$newFecha)){
+				if(preg_match($expreg,$newFecha)){
 					$error=true;
 					$msgError.="<p>La fecha debe de ser del tipo dd/mm/aaaa o dd-mm-aaaa</p>";
 				}
 			}else{
 				$error=true;
 				$msgError.="<p>La fecha debe de ser del tipo dd/mm/aaaa o dd-mm-aaaa</p>";
+			}*/
+			if(($fecha= strtotime($_POST["fecha"]))===false){
+				$error=true;
+				$msgError.="<p>La fecha debe de ser del tipo dd/mm/aaaa o dd-mm-aaaa</p>";
 			}
-		
 		}else{
 			$fecha=null;
 		}
 		
-		$expregNom="/^([0-9]|[a-z]|[A-Z]){3,15}$/";
-		if(!preg_match($expregNom,$_POST["nomUser"])){
-			$error=true;
-			$msgError.="<p>El usuario debe de tener solamente letras mayúsculas, minúsculas y números. Además de ser entre 3 y 15 caracteres.</p>";
+		//Comprobación del nombre de usuario
+		if(isset($_POST["nomUser"])){
+			$expregNom="/^([0-9]|[a-z]|[A-Z]){3,15}$/";
+			if(!preg_match($expregNom,$_POST["nomUser"])){
+				$error=true;
+				$msgError.="<p>El usuario debe de tener solamente letras mayúsculas, minúsculas y números. Además de ser entre 3 y 15 caracteres.</p>";
+			}else{
+				$nombre = $_POST["nomUser"];
+			}
 		}else{
-			$nombre = $_POST["nomUser"];
+			$error=true;
+			$msgError.="<p>Debes de escribir un nombre de usuario.</p>";
 		}
 		
+		//Comprobación de la contraseña
+		if(isset($_POST["pass"])){
 		$expregPass="/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([0-9]|[a-z]|[A-Z]|_){6,15}$/";
-		if(!preg_match($expregNom,$_POST["pass"])){
-			$error=true;
-			$msgError.="<p>La contraseña debe de tener solamente letras mayúsculas, minúsculas y números. Además de ser entre 6 y 15 caracteres.
-			Debe de contener al menos una letra minúscula, una mayúscula y un número.</p>";
-		}else{
-			$pass = $_POST["pass"];
-		}
-		
-		if($_POST["pass"] == $_POST["repPass"]){
-			$pass = $_POST["pass"];
+			if(!preg_match($expregPass,$_POST["pass"])){
+				$error=true;
+				$msgError.="<p>La contraseña debe de tener solamente letras mayúsculas, minúsculas y números. Además de ser entre 6 y 15 caracteres.
+				Debe de contener al menos una letra minúscula, una mayúscula y un número.</p>";
+			}else{
+				$pass = $_POST["pass"];
+			}
 		}else{
 			$error=true;
-			$msgError.="<p>La contraseña no se ha repetido correctamente</p>";
+			$msgError.="<p>Debes de escribir una contraseña.</p>";
 		}
 		
+		//Comprobación de repetir contraseña
+		if(isset($_POST["repPass"])){
+			if($_POST["pass"] == $_POST["repPass"]){
+				$pass = $_POST["pass"];
+			}else{
+				$pass=null;
+				$error=true;
+				$msgError.="<p>La contraseña no se ha repetido correctamente</p>";
+			}		
+		}else{
+			$error=true;
+			$msgError.="<p>Debes repetir la contraseña y deben de ser iguales.</p>";
+		}
+
+		
+		//Asignación de variables restantes
+		if(isset($_POST["correo"])){
+			$expregEmail="/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})$/";
+			if(!preg_match($expregEmail,$_POST["correo"])){
+				$error=true;
+				$msgError.="<p>El correo debe de tener una arroba, y los dominios no pueden ser menores que 2 y mayores de 4 letras.</p>";
+			}else{
+				$correo = $_POST["correo"];
+			}
+		}else{
+			$error=true;
+			$msgError.="<p>Debes de escribir una cuenta de correo para que se te pueda identificar.</p>";
+		}
+
 		$email = $_POST["correo"];
 		$sexo = $_POST["sexo"];
-		$fecha = $_POST["fecha"];
 		$pais = $_POST["paisRegis"];
 		
+		//Sanear el tipo ciudad
 		if(isset($_POST["ciudad"])){
 			$ciudad = $_POST["ciudad"];
 			filter_var($ciudad,FILTER_SANITIZE_STRING);		
@@ -120,18 +170,28 @@
 			$ciudad=null;
 		}
 
-		
+		//Fecha de registro actual
 		$fregistro= date("Y-m-d H:i:s");
 		
+		//Sentencias
 		$sentencia="SELECT * from nothing";
 		if(!$error){
-			$sentencia= "INSERT INTO usuarios VALUES (null,'".$nombre."','".$pass."','".$email."','".$sexo."','".$newFecha."','".$ciudad."','".$pais."','".$ruta."','".$fregistro."')";
+			$sentencia= "INSERT INTO usuarios VALUES (null,'".$nombre."','".$pass."','".$email."','".$sexo."','".$fecha."','".$ciudad."','".$pais."','".$ruta."','".$fregistro."')";
 		}
-	
+		
+		//Conexión con la base de datos
 		if(!mysqli_query($conexion, $sentencia)){
 			$error=true;
-			$msgError.="<p>No se ha podido crear su cuenta de usuario en la base de datos. Posiblemente exista ya el usuario o la base de datos esté caída. Pruebe con otro nombre más tarde.</p>";
+			$msgError.="<p>No se ha podido crear su cuenta de usuario en la base de datos. Puede que el usuario ya esté cogido.</p>";
 		}
+		mysqli_close($conexion);
+		
+		//Nueva conexión para el select de pais
+		include("conexion.php");
+		$sentencia="SELECT * from paises where ".$_POST["paisRegis"]."=paises.IdPais";
+		$resultado = mysqli_query($conexion, $sentencia);
+		
+		//Emisión del error si hay
 		if($error){
 			echo '<div class="alert">
 					'.$msgError.'
@@ -140,9 +200,10 @@
 			echo "
 			<article class='confirmar'>
 				<h4>Confirma los datos de la petición</h4>
-				<p><b>Nombre:</b>$_POST[nomUser]</p>
-				<p><b>Contraseña:</b> $_POST[pass]</p>
-				<p><b>Correo electrónico:</b> $_POST[correo]</p>
+				<p><b>Nombre: </b>$_POST[nomUser]</p>
+				<p><b>Contraseña: </b> $_POST[pass]</p>
+				<p><b>Correo electrónico: </b> $_POST[correo]</p>
+				<p><b>Ciudad: </b> $_POST[ciudad]</p>
 				";
 				if($_POST['sexo']==1){
                     $sexo="Hombre";
@@ -150,10 +211,13 @@
                 else{
                     $sexo="Mujer";
                 }
-			echo"<p><b>Sexo:".$sexo."</b></p>
-				<p><b>Fecha de nacimiento:</b> $_POST[fecha]</p>
-				<p><b>País:</b> $_POST[paisRegis]</p>
-				<p><b>Foto de perfil</b> <img src='".$ruta."'</p>
+			echo "<p><b>Sexo: ".$sexo."</b></p>
+				<p><b>Fecha de nacimiento: </b> $_POST[fecha]</p>
+				";
+				while($fila=mysqli_fetch_assoc($resultado)){
+					echo "<p><b>País: </b> ".$fila["NomPais"]."</p>";
+				}
+			echo "<p><b>Foto de perfil</b> <img src='".$ruta."'</p>
 				<a href='index.php'>Confirmar</a>
 				<a href='index.php'>Cancelar</a>
 			</article>";
@@ -162,6 +226,8 @@
 	</main>
 	<?php
 		include("footer.html");
+		mysqli_free_result($resultado);
+		mysqli_close($conexion);
 	?>
 
 </body>
